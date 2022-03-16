a28853f1999c8360374fe8cf97e91cf810ad1abf
tomhagan29 <tomhagan29@gmail.com>
2022-03-16 16:43:25 +0000

    Practice

---
+++ Practice1.pl
@@ -0,0 +1,11 @@
+sentence --> noun_phrase, verb_phrase.
+noun_phrase --> determiner, noun.
+verb_phrase --> verb, noun_phrase.
+verb_phrase --> verb, sentence.
+
+determiner --> [the].
+determiner --> [a].
+noun --> [cat].
+noun --> [mouse].
+verb --> [scares].
+verb --> [hates].
