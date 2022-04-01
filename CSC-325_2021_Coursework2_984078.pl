e66ac07d52f8cabdc6639ae77a10a8e257d0e4fe
tomhagan29 <tomhagan29@gmail.com>
2022-04-01 12:56:34 +0100

    Update CSC-325_2021_Coursework2_984078.pl

--- CSC-325_2021_Coursework2_984078.pl
+++ CSC-325_2021_Coursework2_984078.pl
@@ -1,19 +1,11 @@
-s(s(NP, VP))   -->  np(NP, Num), vp(VP, Num).
-
-np(np(DET, N), Num) -->  det(DET, Num), n(N, Num).
-np(np(DET, N), Num) -->  n(N, Num).
-
-vp(vp(V, NP), Num)   -->  v(V, Num), np(NP, _).
-vp(vp(V), Num)      -->  v(V, Num).
-
-det(det(the), _)  -->  [the].
-det(det(a), singular)    -->  [a].
-det(det(two), plural) -->[two].
-
-n(n(woman), singular)    -->  [woman].
-n(n(woman), plural)    -->  [women].
-n(n(man), singular)      -->  [man].
-n(n(men), plural)    -->  [men].
-
-v(v(hires), singular)    -->  [hires].
-v(v(hires), plural)    -->  [hire].
+s --> np, vp.
+np --> det, nbar.
+vp --> v, np.
+nbar --> adj, n.
+det --> [the].
+v --> [chased].
+adj --> [angry].
+adj --> [frightened].
+adj --> [little].
+n --> [bear].
+n --> [squirrel].
