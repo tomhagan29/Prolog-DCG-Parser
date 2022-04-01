760bbda768c0d919db83e3bea0c3e2e990bfd6f2
tomhagan29 <tomhagan29@gmail.com>
2022-04-01 20:13:41 +0100

    Included nbar, pp, jp, pro

--- CSC-325_2021_Coursework2_984078.pl
+++ CSC-325_2021_Coursework2_984078.pl
@@ -1,11 +1,24 @@
-s --> np, vp.
-np --> det, nbar.
-vp --> v, np.
-nbar --> adj, n.
-det --> [the].
-v --> [chased].
-adj --> [angry].
-adj --> [frightened].
-adj --> [little].
-n --> [bear].
-n --> [squirrel].
+s(s(NP, VP)) --> np(NP), vp(VP).
+np(np(DET, NBAR)) --> det(DET), nbar(NBAR).
+np(np(PRO)) --> pro(PRO).
+vp(vp(V, NP)) --> v(V), np(NP).
+vp(vp(V)) --> v(V).
+nbar(nbar(N)) --> n(N).
+nbar(nbar(JP)) --> jp(JP).
+pp(pp(PREP, NP)) --> prep(PREP), np(NP).
+jp(jp(ADJ, N)) --> adj(ADJ), n(N).
+
+det(det(Word)) --> [Word], {lex(Word, det)}.
+pro(pro(Word)) --> [Word], {lex(Word, pro)}.
+v(v(Word)) --> [Word], {lex(Word, v)}.
+adj(adj(Word)) --> [Word], {lex(Word, adj)}.
+n(n(Word)) --> [Word], {lex(Word, n)}.
+prep(prep(Word)) --> [Word], {lex(Word, prep)}.
+
+lex(the, det).
+lex(chased, v).
+lex(angry, adj).
+lex(frightened, adj).
+lex(little, adj).
+lex(bear, n).
+lex(squirrel, n).
