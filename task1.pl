463c81c8359293866f4dd856689fd3f6357a88a1
tomhagan29 <tomhagan29@gmail.com>
2022-04-03 19:14:38 +0100

    Update task1.pl

--- task1.pl
+++ task1.pl
@@ -9,8 +9,8 @@ np(np(NBAR)) --> nbar(NBAR).
 np(np(PRO)) --> pro(PRO).
 np(np(PP)) --> pp(PP).
 
-vp(vp(V, NP)) --> v(V), np(NP).
-vp(vp(V)) --> v(V).
+vp(vp(TV, NP)) --> tv(TV), np(NP).
+vp(vp(IV)) --> iv(IV).
 
 nbar(nbar(N)) --> n(N).
 nbar(nbar(JP)) --> jp(JP).
@@ -22,24 +22,79 @@ jp(jp(ADJ, N)) --> adj(ADJ), n(N).
 
 det(det(Word)) --> [Word], {lex(Word, det)}.
 pro(pro(Word)) --> [Word], {lex(Word, pro)}.
-v(v(Word)) --> [Word], {lex(Word, v)}.
-%% iv(iv(Word)) --> [Word], {lex(Word, iv)}.
+tv(tv(Word)) --> [Word], {lex(Word, tv)}.
+iv(iv(Word)) --> [Word], {lex(Word, iv)}.
 adj(adj(Word)) --> [Word], {lex(Word, adj)}.
 n(n(Word)) --> [Word], {lex(Word, n)}.
 prep(prep(Word)) --> [Word], {lex(Word, prep)}.
 
-lex(she, pro).
-lex(knows, v).
-lex(her, pro).
-lex(the, det).
-lex(woman, n).
-lex(on, prep).
-lex(two, det).
-lex(in, prep).
-lex(a, det).
-lex(room, n).
-lex(sees, v).
-lex(tall, adj).
-lex(young, adj).
-lex(men, n).
-lex(chairs, n).
+%% Lexicons
+
+%% Pronouns
+lex(i,pro).
+lex(you,pro).
+lex(he,pro).
+lex(she,pro).
+lex(it,pro).
+lex(we,pro).
+lex(you,pro).
+lex(they,pro).
+lex(me,pro).
+lex(you,pro).
+lex(him,pro).
+lex(her,pro).
+lex(it,pro).
+lex(us,pro).
+lex(you,pro).
+lex(them,pro).
+
+%% Verbs
+lex(know,tv).
+lex(know,tv).
+lex(knows,tv).
+lex(know,tv).
+lex(see,tv).
+lex(see,tv).
+lex(sees,tv).
+lex(see,tv).
+lex(hire,tv).
+lex(hire,tv).
+lex(hires,tv).
+lex(hire,tv).
+lex(fall,iv).
+lex(fall,iv).
+lex(falls,iv).
+lex(fall,iv).
+lex(sleep,iv).
+lex(sleep,iv).
+lex(sleeps,iv).
+lex(sleep,iv).
+
+%% Determiners
+lex(the,det).
+lex(a,det).
+lex(two,det).
+
+%% Nouns
+lex(man,n).
+lex(woman,n).
+lex(apple,n).
+lex(chair,n).
+lex(room,n).
+lex(men,n).
+lex(women,n).
+lex(apples,n).
+lex(chairs,n).
+lex(rooms,n).
+
+%% Preposition
+lex(on,prep).
+lex(in,prep).
+lex(under,prep).
+
+%% Adjective
+lex(old,adj).
+lex(young,adj).
+lex(red,adj).
+lex(short,adj).
+lex(tall,adj).
