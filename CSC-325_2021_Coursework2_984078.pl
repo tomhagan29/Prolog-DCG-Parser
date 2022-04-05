6bda85d248bbd363dc1a04c6e840d7080e5d4958
tomhagan29 <tomhagan29@gmail.com>
2022-04-05 18:24:21 +0100

    Added POV for nouns

--- CSC-325_2021_Coursework2_984078.pl
+++ CSC-325_2021_Coursework2_984078.pl
@@ -1,28 +1,28 @@
 s(s(NP, VP)) --> np(NP, Num, Person, subject, Animacy), vp(VP, Num, Person, object, Animacy).
 
-np(np(DET, NBAR, PP), Num, _, _, Animacy) --> 
-    det(DET, Num), nbar(NBAR, Num, Animacy), pp(PP, Num, _, _, Animacy).
+np(np(DET, NBAR, PP), Num, Person, _, Animacy) --> 
+    det(DET, Num), nbar(NBAR, Num, Person, Animacy), pp(PP, Num, _, _, Animacy).
 
-np(np(DET, NBAR), Num, _, _, Animacy) --> 
-    det(DET, Num), nbar(NBAR, Num, Animacy).
+np(np(DET, NBAR), Num, Person, _, Animacy) --> 
+    det(DET, Num), nbar(NBAR, Num, Person, Animacy).
 
 np(np(PRO), Num, Person, SO, _) --> pro(PRO, Num, Person, SO).
 
 vp(vp(TV, NP), Num, Person, SO, Animacy) --> v(TV, Num, Person, Animacy), np(NP, _, _, SO, _).
 vp(vp(IV), Num, Person, _, Animacy) --> v(IV, Num, Person, Animacy).
 
-nbar(nbar(N), Num, Animacy) --> n(N, Num, Animacy).
-nbar(nbar(JP), Num, Animacy) --> jp(JP, Num, Animacy).
+nbar(nbar(N), Num, Person, Animacy) --> n(N, Num, Person, Animacy).
+nbar(nbar(JP), Num, Person, Animacy) --> jp(JP, Num, Person, Animacy).
 
 pp(pp(PREP, NP), _, _, _, _) --> prep(PREP), np(NP, _, _, _, _).
 
-jp(jp(ADJ, JP), Num, Animacy) --> adj(ADJ), jp(JP, Num, Animacy).
-jp(jp(ADJ, N), Num, Animacy) --> adj(ADJ), n(N, Num, Animacy).
+jp(jp(ADJ, JP), Num, Person, Animacy) --> adj(ADJ), jp(JP, Num, Person, Animacy).
+jp(jp(ADJ, N), Num, Person, Animacy) --> adj(ADJ), n(N, Num, Person, Animacy).
 
 pro(pro(Word), Num, Person, SO) --> [Word], {lex(Word, pro, Num, Person, SO)}.
 v(v(Word), Num, Person, Animacy) --> [Word], {lex(Word, tv, Num, Person, Animacy)}.
 v(v(Word), Num, Person, Animacy) --> [Word], {lex(Word, iv, Num, Person, Animacy)}.
-n(n(Word), Num, Animacy) --> [Word], {lex(Word, n, Num, Animacy)}.
+n(n(Word), Num, Person, Animacy) --> [Word], {lex(Word, n, Num, Person, Animacy)}.
 det(det(Word), Num) --> [Word], {lex(Word, det, Num)}.
 
 adj(adj(Word)) --> [Word], {lex(Word, adj)}.
@@ -48,6 +48,21 @@ lex(it,pro,singular,3,object).
 lex(us,pro,plural,1,object).
 lex(you,pro,plural,2,object).
 lex(them,pro,plural,3,object).
+
+%% Nouns
+%% Added Person to nouns to solve conflicts not included in test cases
+%% i.e. s(Tree, [the, man, sleep], [])
+
+lex(man,n,singular,3,sentient).
+lex(woman,n,singular,3,sentient).
+lex(apple,n,singular,3,insentient).
+lex(chair,n,singular,3,insentient).
+lex(room,n,singular,3,insentient).
+lex(men,n,plural,3,sentient).
+lex(women,n,plural,3,sentient).
+lex(apples,n,plural,3,insentient).
+lex(chairs,n,plural,3,insentient).
+lex(rooms,n,plural,3,insentient).
 
 %% Verbs
 lex(know,tv,singular,1,sentient).
@@ -76,17 +91,7 @@ lex(the,det,_).
 lex(a,det,singular).
 lex(two,det,plural).
 
-%% Nouns
-lex(man,n,singular,sentient).
-lex(woman,n,singular,sentient).
-lex(apple,n,singular,insentient).
-lex(chair,n,singular,insentient).
-lex(room,n,singular,insentient).
-lex(men,n,plural,sentient).
-lex(women,n,plural,sentient).
-lex(apples,n,plural,insentient).
-lex(chairs,n,plural,insentient).
-lex(rooms,n,plural,insentient).
+
 
 %% Preposition
 lex(on,prep).
